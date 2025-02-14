9334c88f05c8667a1729ec23811eb5dc3794701a
Jt0ny <jtonytipan@gmail.com>
2025-02-14 17:40:50 -0500

    Tarea 14

--- src/components/FormularioComponente.vue
+++ src/components/FormularioComponente.vue
@@ -1,15 +1,45 @@
 !<template>
-  <h1>Formulario Persona</h1>
+  <div class="container">
+    <h1>Formulario Persona</h1>
+    <div class="formulario">
+      <p type="Nombre:"><input type="text" /></p>
+      <p type="Apellido:"><input type="text" /></p>
+      <p type="Fecha Nacimiento:"><input type="text" /></p>
+    </div>
+    <button>Agregar</button>
+  </div>
 </template>
 
 <script>
-import{obtenerPorIdFachada} from '@/client/PersonaCliente.js'
-export default {
-    
-
-}
+export default {};
 </script>
 
 <style>
+.container {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
 
+.formulario {
+  display: grid;
+  flex-direction: center;
+  border: solid 1px black;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+p::before {
+  content: attr(type);
+  width: 100px;
+}
+button {
+  margin-top: 10px;
+  padding: 5px;
+  border-radius: 5px;
+}
+h1 {
+  text-align: center;
+  
+}
 </style>
