f52edeca62a7890c54fd3394e7d10324138eaba9
Jt0ny <jtonytipan@gmail.com>
2025-02-14 19:48:32 -0500

    Taller 36

--- src/client/PersonaCliente.js
+++ src/client/PersonaCliente.js
@@ -2,12 +2,29 @@ import axios from 'axios';
 
  const obtenerPorId =async(id)=>{
     //http://localhost:8080/matriculaAPI/v1.1/personas/6
-  const data =  axios.get(`http://localhost:8080/matriculaAPI/v1.1/persona/${id}`).then(r=>r.data);
+  const data =  axios.get(`http://localhost:8081/matriculaAPI/v1.1/persona/${id}`).then(r=>r.data);
     console.log(data);
     return data;
 }
+  const insertar =async(body)=>{
+    axios.post(`http://localhost:8081/matriculaAPI/v1.1/persona`,body).then(r=>r.data);
+  }
 
+  const actualizar =async(id,body)=>{
+    axios.put(`http://localhost:8081/matriculaAPI/v1.1/persona/${id}`,body).then(r=>r.data);
+  }
+
+  const actualizarParcial =async(id,body)=>{
+    axios.patch(`http://localhost:8081/matriculaAPI/v1.1/persona/${id}`,body).then(r=>r.data);
+  }
+
+  const eliminar =async(id)=>{
+    axios.delete(`http://localhost:8081/matriculaAPI/v1.1/persona/${id}`).then(r=>r.data);
+  }
 //FACHADAS
 export const obtenerPorIdFachada= async(id)=>{
     return await obtenerPorId(id);
+}
+export const insertarFachada= async(body)=>{
+    await insertar(body);
 }
--- src/components/FormularioComponente.vue
+++ src/components/FormularioComponente.vue
@@ -2,16 +2,48 @@
   <div class="container">
     <h1>Formulario Persona</h1>
     <div class="formulario">
-      <p type="Nombre:"><input type="text" /></p>
-      <p type="Apellido:"><input type="text" /></p>
-      <p type="Fecha Nacimiento:"><input type="text" /></p>
+      <p type="Nombre:"><input type="text"  v-model="nombre"/></p>
+      <p type="Apellido:"><input type="text" v-model="apellido"/></p>
+      <p type="Fecha Nacimiento:"><input type="datetime-local" v-model="fechaNacimiento" /></p>
     </div>
-    <button>Agregar</button>
+    <button @click="buscar" >buscar</button>
+    <button @click="guardar" >guardar</button>
   </div>
 </template>
 
 <script>
-export default {};
+import {obtenerPorIdFachada,insertarFachada} from '@/client/PersonaCliente.js';
+ 
+export default {
+  data() {
+    return {
+      idPersona: null,
+      nombre: null,
+      apellido: null,
+      fechaNacimiento: null
+    };
+  },
+  mounted() {
+    console.log("Antes de llamar al API");
+    obtenerPorIdFachada(5);
+  },
+ 
+  methods: {
+   async buscar() {
+      const data=await obtenerPorIdFachada(this.idPersona);
+      this.nombre=data.nombre;
+      this.apellido=data.apellido;
+      this.fechaNacimiento=data.fechaNacimiento;
+    },
+     async guardar(){
+      const bodyPersona={
+        nombre:this.nombre,
+        apellido:this.apellido,
+      }
+      await insertarFachada(bodyPersona);
+    }
+  }
+};
 </script>
 
 <style>
